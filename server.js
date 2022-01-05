d235e56e3a4bb3a7d0ed4b0dfe51371ce2e02fd8
unknown <buchimmaduabuchukwu@gmail.com>
2022-01-05 17:21:16 +0100

    changes

--- server.js
+++ server.js
@@ -10,12 +10,10 @@ const image=require('./controllers/image')
 
 const db=knex({
   client: 'pg',
-  connection: {
-    connectionString: process.env.DATABASE_URL,
+  connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false
      }
-  }
 });
 const app=express();
 app.use(bodyparser.json())
