d19d679cc65c8379a9814ffd12040e300cccc737
unknown <buchimmaduabuchukwu@gmail.com>
2022-01-06 20:07:18 +0100

    commit

--- server.js
+++ server.js
@@ -12,10 +12,11 @@ const db=knex({
   client: 'pg',
   connection: {
      connectionString: process.env.DATABASE_URL
-  }
-  ssl: {
+     ssl: {
     rejectUnauthorized: false
   }
+  }
+
 });
 const app=express();
 app.use(bodyparser.json())
