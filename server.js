ba267fd239024dc4ecf5a2ced32926924285399b
unknown <buchimmaduabuchukwu@gmail.com>
2022-01-06 23:52:13 +0100

    commit

--- server.js
+++ server.js
@@ -11,11 +11,11 @@ const image=require('./controllers/image')
 const db=knex({
   client: 'pg',
   connection: {
-     connectionString: process.env.DATABASE_URL
-  },
+     connectionString: process.env.DATABASE_URL,
   ssl: {
-    rejectUnauthorized: false
-  }
+    rejectUnauthorized: false,
+  },
+},
 });
 const app=express();
 app.use(bodyparser.json())
