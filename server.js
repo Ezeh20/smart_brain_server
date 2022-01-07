c059c0072299e66c1d595dfab042aaa831d74fb2
unknown <buchimmaduabuchukwu@gmail.com>
2022-01-07 01:11:00 +0100

    commit

--- server.js
+++ server.js
@@ -8,15 +8,15 @@ const signin=require('./controllers/signin');
 const profileGet=require('./controllers/profile')
 const image=require('./controllers/image')
 
+const parse = require('pg-connection-string').parse;
+const pgconfig = parse('postgres://veauardkpvqpzw:177570c9728709f064a82a3f481ee35b4e0f367141d5b798ef90b60d3eea0641@ec2-44-193-228-249.compute-1.amazonaws.com:5432/d28fg11sbng21p');
+pgconfig.ssl = { rejectUnauthorized: false };
+
 const db=knex({
   client: 'pg',
-  connection: {
-     connectionString:"postgres://veauardkpvqpzw:177570c9728709f064a82a3f481ee35b4e0f367141d5b798ef90b60d3eea0641@ec2-44-193-228-249.compute-1.amazonaws.com:5432/d28fg11sbng21p",
-  ssl: {
-    rejectUnauthorized: false,
-  },
-},
+  connection: pgconfig
 });
+
 const app=express();
 app.use(bodyparser.json())
 app.use(cors())
